0b8ad997a556240dd573e2c4bc5fb0997e995357
v s <v.s+1775559042@dbtlabsworkshop.com>
2026-04-07 11:58:36 +0000

    merge

---
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -0,0 +1,22 @@
+with 
+
+source as (
+
+    select * from {{ source('jaffle_shop', 'orders_jaffle_shop') }}
+
+),
+
+renamed as (
+
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status,
+        _etl_loaded_at
+
+    from source
+
+)
+
+select * from renamed
